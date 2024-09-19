afa8908f38fa65c8ce1b88a57ce4d02af9ab88e8
Aron L. <aronlovey@gmail.com>
2024-09-19 16:32:29 -0300

    cambiada las keys del diccionario specs a strings, complicaba mucho el codigo tenerlos en enteros

--- modulos/constantes.py
+++ modulos/constantes.py
@@ -12,10 +12,10 @@ ______________  ___
 """
 
 SPECS = {
-    1: "Ciencia de Datos",
-    2: "Back-End",
-    3: "Front-End",
-    4: "UI/UX",
-    5: "Bases de datos",
-    6: "Ciberseguridad"
+    "1": "Ciencia de Datos",
+    "2": "Back-End",
+    "3": "Front-End",
+    "4": "UI/UX",
+    "5": "Bases de datos",
+    "6": "Ciberseguridad"
 }
